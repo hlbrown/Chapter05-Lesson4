2f6a214de3bcc36fe1df656b11fa705f33fe60d5
Hannah Brown <nagle.hannah1@gmail.com>
2021-06-08 16:03:00 -0400

    editted div

--- template.js
+++ template.js
@@ -8,7 +8,7 @@ export default () => {
                 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
             </head>
             <body>
-                <div id="root">Hello World</div>
+                <div id="root"></div>
                 <script type="text/javascript" src="/dist/bundle.js"></script>
             </body>
         </html>`
