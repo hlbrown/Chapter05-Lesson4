33af0df2253b9d2b8b7f521407bb6e677b901228
Hannah Brown <nagle.hannah1@gmail.com>
2021-06-09 13:11:39 -0400

    commit on

--- client/user/Users.js
+++ client/user/Users.js
@@ -1,5 +1,4 @@
-  
-import React, {useState, useEffect} from 'react'
+  import React, {useState, useEffect} from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import List from '@material-ui/core/List'
